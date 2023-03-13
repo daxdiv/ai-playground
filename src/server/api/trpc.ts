ddd468b1bd91f3254a1f0ac6ad2b401587fad35a
daxdiv <david200288l@gmail.com>
2023-03-13 10:19:35 +0100

    first commit

---
+++ src/server/api/trpc.ts
@@ -0,0 +1,23 @@
+import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
+import { initTRPC } from "@trpc/server";
+import superjson from "superjson";
+
+type CreateContextOptions = Record<string, never>;
+
+const createInnerTRPCContext = (_opts: CreateContextOptions) => {
+  return {};
+};
+
+export const createTRPCContext = (_opts: CreateNextContextOptions) => {
+  return createInnerTRPCContext({});
+};
+
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape }) {
+    return shape;
+  },
+});
+
+export const createTRPCRouter = t.router;
+export const publicProcedure = t.procedure;
